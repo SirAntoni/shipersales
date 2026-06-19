9d0d51f0c970eb34f830ba31e40860f1f79da7c7
antony alfredo culqui carranza <antony@192.168.1.27>
2026-06-19 13:28:16 -0500

    fix(pdf): snappy usa la variable WKHTMLTOPDF del .env y limpia comillas del path

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
     
     'pdf' => [
         'enabled' => true,
-        'binary'  => env('WKHTML_PDF_BINARY', '/usr/local/bin/wkhtmltopdf'),
+        'binary'  => trim(env('WKHTMLTOPDF', env('WKHTML_PDF_BINARY', '/usr/local/bin/wkhtmltopdf')), '"'),
         'timeout' => false,
         'options' => [
             'enable-local-file-access' => true,
